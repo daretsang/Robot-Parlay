d79f2121412aa917d0bdf395585e3bd873340d4f
daretsang <daretsang@gmail.com>
2025-05-12 15:08:48 -0700

    Update roboparlay_maker_app.py

--- roboparlay_maker_app.py
+++ roboparlay_maker_app.py
@@ -49,3 +49,47 @@ if league == "MLB":
 
         except Exception as e:
             st.error(f"An error occurred: {e}")
+    
+elif league == "NHL":
+    teamname1 = st.text_input("enter the first team you want to bet on (fromat: vegas-golden-knights)").lower()
+    teamname2 = st.text_input("enter the second team you want to bet on (fromat: winnipeg-jets)").lower()
+    number_legs = st.number_input("How many legs do you want your parlay to be?", min_value=3, max_value=5, step=1)
+
+
+    start = st.button("Generate Parlay")
+    placeholder1 = st.empty()
+    placeholder2 = st.empty()
+    placeholder3 = st.empty()
+    placeholder4 = st.empty()
+    placeholder5 = st.empty()
+
+    lineup_url1 = "https://www.dailyfaceoff.com/teams/" + teamname1 +"/line-combinations"
+    lineup_url2 = "https://www.dailyfaceoff.com/teams/" + teamname2 +"/line-combinations"
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"}
+    players = []
+
+    if start:
+        try:
+            response1 = requests.get(lineup_url1, headers = headers)
+            soup1 = BeautifulSoup(response1.content, 'lxml')
+
+            top6_1 = soup1.find_all('div', class_='flex flex-row justify-center')
+            for player in (top6_1[:6]):
+                players.append(player.text.strip())
+
+            response2 = requests.get(lineup_url2, headers = headers)
+            soup2 = BeautifulSoup(response1.content, 'lxml')
+
+            top6_2 = soup2.find_all('div', class_='flex flex-row justify-center')
+            for player in (top6_2[:6]):
+                players.append(player.text.strip())
+
+            chosen = random.sample(range(1, 13), number_legs)
+
+            counter = 1
+            for choice in chosen:
+                globals()[f'placeholder{counter}'].markdown(players[choice] + " for 1+ Point")
+                counter += 1
+
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
